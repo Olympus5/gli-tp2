c27196b27eae119648ffb4f8bb55e94c61f0f469
Erwan IQUEL <erwan.iquel@gmail.com>
2018-09-25 16:28:43 +0200

    Add some players prototypes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var playerGenerator = require('./playerGenerator').playerGenerator;
+var Player = require('./Player').Player;
+var PayToWinPlayer = require('./PayToWinPlayer').PayToWinPlayer;
 
-var player1 = playerGenerator("erwan", 20, 8),
-    player2 = playerGenerator("roberto", 5, 5);
+var player1 = new Player("erwan", 20, 8),
+    player2 = new PayToWinPlayer("roberto", 19, 8);
 
 player1.displayMyPlayerInfo();
 player2.displayMyPlayerInfo();
